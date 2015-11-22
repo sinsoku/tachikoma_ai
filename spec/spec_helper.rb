a53e159a4aaf8de7b10823712a57d3a8ad2bf51a
sinsoku <sinsoku.listy@gmail.com>
2015-11-22 12:19:26 +0900

    Introduce codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 require 'tachikoma_ai'
 require 'tachikoma_ai/strategies/bundler'
