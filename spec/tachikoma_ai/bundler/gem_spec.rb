f90f2e37c9157e6b2429a7b38d6df8ace7a77cef
sinsoku <sinsoku.listy@gmail.com>
2016-02-29 01:30:36 +0900

    Fix Style/ClassAndModuleChildren

--- spec/tachikoma_ai/bundler/gem_spec.rb
+++ spec/tachikoma_ai/bundler/gem_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
-module TachikomaAi::Bundler
-  describe '.parse' do
-    subject { Gem.parse('+    byebug (5.0.0)') }
-    it { expect(subject.name).to eq 'byebug' }
-    it { expect(subject.version).to eq '5.0.0' }
-  end
+module TachikomaAi
+  module Bundler
+    describe '.parse' do
+      subject { Gem.parse('+    byebug (5.0.0)') }
+      it { expect(subject.name).to eq 'byebug' }
+      it { expect(subject.version).to eq '5.0.0' }
+    end
 
-  describe '#homepage' do
-    let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
-    before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
-    it { expect(gem.homepage).to eq 'https://github.com/sinsoku/tachikoma_ai' }
-  end
+    describe '#homepage' do
+      let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
+      before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
+      it { expect(gem.homepage).to eq 'https://github.com/sinsoku/tachikoma_ai' }
+    end
 
-  describe '#github?' do
-    let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
-    before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
-    it { expect(gem).to be_github }
+    describe '#github?' do
+      let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
+      before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
+      it { expect(gem).to be_github }
+    end
   end
 end
