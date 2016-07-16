08277b69413fe0427fa686e1cc43050d07f90ec9
sinsoku <sinsoku.listy@gmail.com>
2016-07-17 02:19:18 +0900

    :recycle: Adjust module hierarchy

--- lib/tachikoma_ai/strategies/bundler/gem.rb
+++ lib/tachikoma_ai/strategies/bundler/gem.rb
@@ -2,54 +2,56 @@ require 'rubygems'
 require 'json'
 
 module TachikomaAi
-  class Bundler
-    class Gem
-      STRING_PATTERN = /[-|\+]\s+(\S+)\s\((.+?)\)/
-      major, minor = RUBY_VERSION.split('.')
-      SPECS_PATH = "vendor/bundle/ruby/#{major}.#{minor}.0/specifications/".freeze
-      URLS = JSON.parse(File.read(File.expand_path('urls.json', __dir__)))
+  module Strategies
+    class Bundler
+      class Gem
+        STRING_PATTERN = /[-|\+]\s+(\S+)\s\((.+?)\)/
+        major, minor = RUBY_VERSION.split('.')
+        SPECS_PATH = "vendor/bundle/ruby/#{major}.#{minor}.0/specifications/".freeze
+        URLS = JSON.parse(File.read(File.expand_path('urls.json', __dir__)))
 
-      attr_reader :name, :version
-      attr_accessor :from
+        attr_reader :name, :version
+        attr_accessor :from
 
-      def self.parse(s)
-        m = s.match STRING_PATTERN
-        new m[1], m[2]
-      end
-
-      def initialize(name, version)
-        @name = name
-        @version = version
-      end
-
-      def github_url?
-        !github_url.nil?
-      end
-
-      def github_url
-        if URLS.key?(name)
-          URLS[name]
-        elsif homepage && homepage.include?('github')
-          homepage
+        def self.parse(s)
+          m = s.match STRING_PATTERN
+          new m[1], m[2]
         end
-      end
 
-      def homepage
-        @homepage ||= spec.homepage
-      end
+        def initialize(name, version)
+          @name = name
+          @version = version
+        end
 
-      def compare_url
-        GitHub.new(github_url).compare_url(from, version)
-      end
+        def github_url?
+          !github_url.nil?
+        end
 
-      private
+        def github_url
+          if URLS.key?(name)
+            URLS[name]
+          elsif homepage && homepage.include?('github')
+            homepage
+          end
+        end
 
-      def spec
-        ::Gem::Specification.load(spec_path)
-      end
+        def homepage
+          @homepage ||= spec.homepage
+        end
 
-      def spec_path
-        "#{SPECS_PATH}/#{name}-#{version}.gemspec"
+        def compare_url
+          GitHub.new(github_url).compare_url(from, version)
+        end
+
+        private
+
+        def spec
+          ::Gem::Specification.load(spec_path)
+        end
+
+        def spec_path
+          "#{SPECS_PATH}/#{name}-#{version}.gemspec"
+        end
       end
     end
   end
--- spec/tachikoma_ai/strategies/bundler/gem_spec.rb
+++ spec/tachikoma_ai/strategies/bundler/gem_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 module TachikomaAi
-  class Bundler
-    describe Gem do
-      describe '.parse' do
-        subject { Gem.parse('+    byebug (5.0.0)') }
-        it { expect(subject.name).to eq 'byebug' }
-        it { expect(subject.version).to eq '5.0.0' }
-      end
-
-      describe '#homepage' do
-        let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
-        before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
-        it { expect(gem.homepage).to eq 'https://github.com/sinsoku/tachikoma_ai' }
-      end
-
-      describe '#github_url?' do
-        context 'gem does not include in URLS' do
-          let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
-          before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
-          it { expect(gem).to be_github_url }
+  module Strategies
+    class Bundler
+      describe Gem do
+        describe '.parse' do
+          subject { Gem.parse('+    byebug (5.0.0)') }
+          it { expect(subject.name).to eq 'byebug' }
+          it { expect(subject.version).to eq '5.0.0' }
         end
 
-        context 'gem include in URLS' do
-          let(:gem) { Gem.new('sidekiq', '1.0.0') }
-          it { expect(gem).to be_github_url }
+        describe '#homepage' do
+          let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
+          before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
+          it { expect(gem.homepage).to eq 'https://github.com/sinsoku/tachikoma_ai' }
+        end
+
+        describe '#github_url?' do
+          context 'gem does not include in URLS' do
+            let(:gem) { Gem.new('tachikoma_ai', '0.1.0') }
+            before { allow(gem).to receive(:spec_path) { 'tachikoma_ai.gemspec' } }
+            it { expect(gem).to be_github_url }
+          end
+
+          context 'gem include in URLS' do
+            let(:gem) { Gem.new('sidekiq', '1.0.0') }
+            it { expect(gem).to be_github_url }
+          end
         end
       end
     end
