110f6794b1df6f89f9551355fb7d14c235e95b0e
Satoshi Ohmori <sachin21dev@gmail.com>
2017-05-22 11:46:32 +0900

    Fix a bug where the previous gem does not exist

--- spec/tachikoma_ai/strategies/bundler_spec.rb
+++ spec/tachikoma_ai/strategies/bundler_spec.rb
@@ -5,9 +5,6 @@ module TachikomaAi
     describe Bundler do
       describe '#pull_request_body' do
         let(:bundler) { Bundler.new }
-        let(:expected) { 'https://github.com/sinsoku/tachikoma_ai/compare/v0.1.0...v0.2.0' }
-        let(:previous) { ::Bundler::LockfileParser.new("GEM\n  specs:\n    tachikoma_ai (0.1.0)") }
-        let(:current) { ::Bundler::LockfileParser.new("GEM\n  specs:\n    tachikoma_ai (0.2.0)") }
 
         before do
           allow(bundler).to receive(:lockfile).and_return(previous, current)
@@ -15,7 +12,31 @@ module TachikomaAi
           stub_request(:get, 'https://api.github.com/repos/sinsoku/tachikoma_ai/git/refs/tags')
             .to_return(status: 200, body: '[{"ref":"refs/tags/v0.1.0"}, {"ref":"refs/tags/v0.2.0"}]')
         end
-        it { expect(bundler.pull_request_body).to include expected }
+
+        context 'when successful' do
+          let(:expected) { 'https://github.com/sinsoku/tachikoma_ai/compare/v0.1.0...v0.2.0' }
+
+          let(:previous) { ::Bundler::LockfileParser.new("GEM\n  specs:\n    tachikoma_ai (0.1.0)") }
+          let(:current) { ::Bundler::LockfileParser.new("GEM\n  specs:\n    tachikoma_ai (0.2.0)") }
+
+          it { expect(bundler.pull_request_body).to include expected }
+        end
+
+        context 'when unsuccessful' do
+          let(:expected) { 'activesupport' }
+          let(:previous) do
+            ::Bundler::LockfileParser.new(
+              "GEM\n  specs:\n    activesupport (4.2.6)\n      i18n (~> 0.7)"
+            )
+          end
+          let(:current) do
+            ::Bundler::LockfileParser.new(
+              "GEM\n  specs:\n    activesupport (5.1.1)\n      concurrent-ruby (~> 1.0, >= 1.0.2)"
+            )
+          end
+
+          it { expect(bundler.pull_request_body).not_to include expected }
+        end
       end
     end
   end
